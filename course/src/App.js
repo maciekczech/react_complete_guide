3343f8650b9c87a1775f021ca52524857b67b64d
Maciej <czechmaciej66@gmail.com>
2021-01-07 14:01:31 +0100

    Starting section 4

--- course/src/App.js
+++ course/src/App.js
@@ -70,16 +70,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-  
-        <button onClick={this.togglePersonHandler}>Switch name</button>
-        
+        <button 
+        className = "Button"
+        onClick={this.togglePersonHandler}>
+          Hide everything
+        </button>
         {persons}
       </div>
     );
